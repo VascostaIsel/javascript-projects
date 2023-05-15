fb7321028295e069eb6b65b455166ea3a04cb7c2
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-05-15 17:34:11 +0100

    Started Rock, Paper, Scissors Game

---
+++ Rock, Paper, Scissors Game/script.js
@@ -0,0 +1,10 @@
+function displayDate() {
+    let plays = ["Rock", "Paper", "Scissors"]
+
+    
+    
+    document.getElementById("date").innerHTML = day + "/" + month + "/" + year
+    document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds + " " + session
+}
+
+document.addEventListener()
