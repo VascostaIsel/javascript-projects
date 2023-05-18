cdab6d07795bb0bf2a874d1bf3a33a73918bf7b1
Vasco Costa <101342648+VascostaIsel@users.noreply.github.com>
2023-05-18 18:45:42 +0100

    Added images for user

--- Rock, Paper, Scissors Game/script.js
+++ Rock, Paper, Scissors Game/script.js
@@ -13,17 +13,17 @@ function playGame(playerChoice) {
 
     let result
     if (playerChoice === randomChoice) {
-        result = "It's a tie!"
+        result = "It's a draw!"
     } else if (
         (playerChoice === "rock" && randomChoice === "scissors") ||
         (playerChoice === "paper" && randomChoice === "rock") ||
         (playerChoice === "scissors" && randomChoice === "paper")
     ) {
-        result = "You win!"
+        result = "CPU won!"
         userScore++
         document.getElementById("user-score").innerHTML = userScore
     } else {
-        result = "You lose!"
+        result = "User won!"
         cpuScore++
         document.getElementById("cpu-score").innerHTML = cpuScore
     }
@@ -31,13 +31,24 @@ function playGame(playerChoice) {
     document.getElementById("result").innerHTML = result
 }
 
+const rockImg = "fas fa-hand-rock"
+const paperImg = "fas fa-hand-paper"
+const scissorsImg = "fas fa-hand-scissors"
+let lastUserImg = rockImg
+
 function playRock() { 
+    document.getElementsByClassName(lastUserImg)[0].className = rockImg
+    lastUserImg = rockImg
     playGame("rock")
 }
 function playPaper() {
+    document.getElementsByClassName(lastUserImg)[0].className = paperImg
+    lastUserImg = paperImg
     playGame("paper")
 }
 function playScissors() {
+    document.getElementsByClassName(lastUserImg)[0].className = scissorsImg
+    lastUserImg = scissorsImg
     playGame("scissors")
 }
 
